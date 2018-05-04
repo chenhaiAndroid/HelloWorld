8dbde5011d61aefe44781c5e229974ff8677e34e
chenhaiAndroid <752086520@qq.com>
2018-05-04 16:10:56 +0800

    haha

--- app/src/main/java/test/yunlianhui/com/myapplication/MainActivity.java
+++ app/src/main/java/test/yunlianhui/com/myapplication/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     private String text;
+    private int chenhai;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
