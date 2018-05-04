0ae1b7c7c8c7d73f96d95838837e9f8d826a1803
chenhaiAndroid <752086520@qq.com>
2018-05-04 16:10:13 +0800

    haha

--- app/src/main/java/test/yunlianhui/com/myapplication/MainActivity.java
+++ app/src/main/java/test/yunlianhui/com/myapplication/MainActivity.java
@@ -5,10 +5,13 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String text;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //chenhai
+
     }
 }
